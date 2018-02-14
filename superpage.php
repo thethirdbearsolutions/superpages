582dcad23c1329495337850efa6f22aea8da9fd6
Ethan Jucovy <ethan.jucovy@gmail.com>
2018-02-14 07:38:44 -0500

    ACF Pro v5 compatibility -- ensure globally unique keys, move giant blob to json file

--- superpage.php
+++ superpage.php
@@ -3,7 +3,7 @@
 Plugin Name: Super Pages
 Plugin URI: http://walihassan.com
 Description: Super Pages Plugin
-Version: 1.1.7
+Version: 1.2.0
 Author: Wali Hassan, Matthew Anderson
 Author URI: http://walihassan.com
 GitHub Plugin URI: https://github.com/350org/superpages
